cf84edd781c96a44c93636b46757a52e02244ce5
Panda-1337 <wish@skeet.group>
2020-07-29 11:18:05 +0800

    Create cc.sh

---
+++ cc.sh
@@ -0,0 +1,6 @@
+yum install python2
+sudo ln -s /usr/bin/python2.7 /usr/bin/python
+wget https://raw.githubusercontent.com/jseidl/GoldenEye/master/goldeneye.py -O goldeneye.py
+chmod +x goldeneye.py
+echo "安装完成，黄金眼SH NY PANDA"
+echo "使用方法： ./goldeneye.py http://xxx"
